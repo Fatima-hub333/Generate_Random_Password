44cd601a77fb2b8d579aad9775d96b6f8ba36a74
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-05-30 11:31:16 +0500

    Update Index.html

---
+++ index.js
@@ -0,0 +1,5 @@
+// Declares and store possible characters in a variable called chars
+let chars = "0123456789abcdefghiklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+// Declares a variable called passwordLength and assign its value to 8
+let passwordLength = 8;
