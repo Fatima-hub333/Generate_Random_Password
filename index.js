d114a98a374b070bd609600a98ae2a78462a6c3c
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-05-31 12:21:19 +0500

    Fix linter errors

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 // Declares and store possible characters in a variable called chars
-let chars =
-  "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const chars = '0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 // Declares a variable called passwordLength and assign its value to 12
-let passwordLength = 8;
+const passwordLength = 8;
 
 // Stores the password1-el paragraph in a variable called password1EL
-let password1El = document.getElementById("password1-el");
-let password2El = document.getElementById("password2-el");
-let password3El = document.getElementById("password3-el");
-let password4El = document.getElementById("password4-el");
+const password1El = document.getElementById('password1-el');
+const password2El = document.getElementById('password2-el');
+const password3El = document.getElementById('password3-el');
+const password4El = document.getElementById('password4-el');
 
+// eslint-disable-next-line max-len
 // Creates a function generatePasswords() that return a random password composed of passwordLength characters
 function generatePasswords() {
   // Declares a variable called password and assign its value to an empty string
-  let password = "";
+  let password = '';
+  // eslint-disable-next-line no-plusplus
   for (let j = 0; j < passwordLength; j++) {
-    let randomIndex = Math.floor(Math.random() * chars.length);
+    const randomIndex = Math.floor(Math.random() * chars.length);
 
     // Adds the new random chars to the password string
     password += chars[randomIndex];
@@ -25,6 +26,7 @@ function generatePasswords() {
 }
 
 // Creates a function getGeneratePasswords() that displays generated passwords in HTML document
+// eslint-disable-next-line no-unused-vars
 function getGeneratePasswords() {
   // Displays the message in the messageEl using messageEl.textContent
   password1El.textContent = generatePasswords();
@@ -32,15 +34,17 @@ function getGeneratePasswords() {
   password3El.textContent = generatePasswords();
   password4El.textContent = generatePasswords();
   // Call changeColorPasswords() function
+  // eslint-disable-next-line no-use-before-define
   changeColorPasswords();
 }
 
 // Creates a function changeColorPasswords() that changes color to passwords
 function changeColorPasswords() {
   // Gets and stores all elements with generatedPassword class in allPassword variable
-  let allPasswords = document.getElementsByClassName("generatedPassword");
+  const allPasswords = document.getElementsByClassName('generatedPassword');
   // Changes color to var(--clr-bright-green) for every passwords in allPasswords
+  // eslint-disable-next-line no-plusplus
   for (let i = 0; i < allPasswords.length; i++) {
-    allPasswords[i].style.color = "var(--clr-bright-green)";
+    allPasswords[i].style.color = 'var(--clr-bright-green)';
   }
 }
